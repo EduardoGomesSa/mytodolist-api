55822a08268f24c9d62973dd26c809658fd0de07
EduardoGomesSa <eduardo.sa.022@gmail.com>
2024-06-05 17:29:31 -0300

    preenchimento do fillable de task e items

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'start_date',
+        'end_date',
+        'status',
+    ];
 }
