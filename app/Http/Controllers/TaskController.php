151659ccd20000710036cf2a1830b951c34a73a6
EduardoGomesSa <eduardo.sa.022@gmail.com>
2024-06-05 19:31:10 -0300

    criação dos metodos post e index de TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    protected $task;
+
+    public function __construct(Task $task) {
+        $this->task = $task;
+    }
+
+    public function index(){
+        return TaskResource::collection(
+            $this->task->all(),
+        );
+    }
+
+    public function store(TaskRequest $request){
+        $task = $this->task->create($request->all());
+
+        if($task == null) return response(['error'=>'task wasn´t created'])->setStatusCode(401);
+
+        $resource = new TaskResource($task);
+
+        return $resource->response()->setStatusCode(201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
