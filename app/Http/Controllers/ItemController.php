51e232161f87d4dd02076027639d33062a6e66e2
EduardoGomesSa <eduardo.sa.022@gmail.com>
2024-06-07 23:30:20 -0300

    criação do ItemResource

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    private $item;
+
+    public function __construct(Item $item) {
+        $this->item = $item;
+    }
+
+    public function index(){
+        
+    }
 }
