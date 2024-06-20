3e259e9ed7fd29b2a3500bc4a7f24fb4e0422eba
EduardoGomesSa <eduardo.sa.022@gmail.com>
2024-06-20 01:42:30 -0300

    Lógica do método store transferida para  TaskRepository

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -4,22 +4,47 @@ namespace App\Repositories;
 
 use App\Models\Task;
 
-class TaskRepository{
+class TaskRepository
+{
     private Task $task;
 
-    public function __construct(Task $task) {
+    public function __construct(Task $task)
+    {
         $this->task = $task;
     }
 
-    public function index(){
+    public function index()
+    {
         return $this->task->all();
     }
 
-    public function getById(int $id){
+    public function getById(int $id)
+    {
         $task = $this->task->find($id);
 
-        if(!$task) return null;
+        if (!$task) return null;
 
         return $task;
+    }
+
+    public function store(Task $task)
+    {
+        $taskCreated = $this->task->create([
+            'name' => $task->name,
+            'description'=> $task->description,
+            'start_date'=> $task->start_date,
+            'end_date'=> $task->end_date ? $task->end_date : null,
+            'status'=> $task->status,
+        ]);
+
+        if(!$taskCreated) return null;
+
+        $taskCreated->items()->createMany($task->items);
+
+        // if ($task->items->isNotEmpty()) {
+        //     $taskCreated->items()->createMany($task->items);
+        // }
+
+        return $taskCreated;
     }
 }
--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -7,72 +7,107 @@ use App\Http\Requests\TaskRequest;
 use App\Http\Requests\TaskUpdateRequest;
 use App\Http\Requests\TaskUpdateStatusRequest;
 use App\Http\Resources\TaskResource;
+use App\Models\Item;
 use App\Models\Task;
 use App\Repositories\TaskRepository;
 
-class TaskService{
+class TaskService
+{
     private $task;
     private TaskRepository $repository;
 
-    public function __construct(Task $task, TaskRepository $repository) {
+    public function __construct(Task $task, TaskRepository $repository)
+    {
         $this->task = $task;
         $this->repository = $repository;
     }
 
-    public function index(){
+    public function index()
+    {
         return TaskResource::collection(
             $this->repository->index(),
         );
     }
 
-    public function store(TaskRequest $request){
-        $taskCreated = $this->task->create($request->all());
+    public function store(TaskRequest $request)
+    {
+        $taskCreated = $this->repository->store($this->convertToCreate($request));
 
-        if(!$taskCreated) null;
-        //if(!$taskCreated) return response(['error'=>'task does not was created'])->setStatusCode(401);
-
-        if($request->items){
-            $taskCreated->items()->createMany($request->items);
-        }
+        if (!$taskCreated) null;
 
         $resource = new TaskResource($taskCreated);
 
         return $resource;
     }
 
-    public function update(TaskUpdateRequest $request){
+    public function update(TaskUpdateRequest $request)
+    {
         $taskExist = $this->repository->getById($request->id);
 
-        if(!$taskExist) return false;
+        if (!$taskExist) return false;
 
         $taskUpdated = $taskExist->update($request->all());
 
-        if($taskUpdated > 0) return true;
+        if ($taskUpdated > 0) return true;
 
         return false;
     }
 
-    public function updateStatus(TaskUpdateStatusRequest $request){
+    public function updateStatus(TaskUpdateStatusRequest $request)
+    {
         $taskExist = $this->repository->getById($request->id);
 
-        if($taskExist == null) return false;
+        if ($taskExist == null) return false;
 
         $taskUpdated = $taskExist->update($request->all());
 
-        if($taskUpdated > 0) return true;
+        if ($taskUpdated > 0) return true;
 
         return false;
     }
 
-    public function destroy(TaskDeleteRequest $request){
+    public function destroy(TaskDeleteRequest $request)
+    {
         $taskExist = $this->repository->getById($request->id);
 
-        if(!$taskExist) return false;
+        if (!$taskExist) return false;
 
         $taskDeleted = $taskExist->delete();
 
-        if($taskDeleted > 0) return true;
-        
+        if ($taskDeleted > 0) return true;
+
         return false;
+    }
+
+    private function convertToCreate(TaskRequest $request): Task
+    {
+        $task = new Task([
+            'name' => $request->name,
+            'description' => $request->description,
+            'start_date' => $request->start_date,
+            'status' => $request->status,
+        ]);
+
+        if ($request->filled('end_date')) {
+            $task->end_date = $request->end_date;
+        }
+
+        if ($request->filled('items')) {
+            $items = [];
+
+            foreach ($request->input('items') as $item) {
+                $item = new Item([
+                    'name' => $item['name'],
+                    'description' => $item['description'],
+                    'status' => $item['status'],
+                ]);
+
+                $items[] = $item->toArray();
+            }
+
+            $task->setRelation('items', $items);
+        }
+
+        return $task;
     }
 }
