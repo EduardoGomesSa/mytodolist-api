4f1887153dd44088af25b7c261e9215d32003266
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-10-22 23:11:50 -0300

    transferencia do codigo de TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TaskDeleteRequest;
+use App\Http\Requests\TaskRequest;
+use App\Http\Requests\TaskUpdateRequest;
+use App\Http\Requests\TaskUpdateStatusRequest;
+use App\Services\TaskService;
 
 class TaskController extends Controller
 {
-    //
+    private $service;
+
+    public function __construct(TaskService $service) {
+        $this->service = $service;
+    }
+
+    public function index(){
+        return $this->service->index();
+    }
+
+    public function store(TaskRequest $request){
+        $taskCreated = $this->service->store($request);
+
+        if(!$taskCreated) return response(['error'=>'task does not was created'])->setStatusCode(401);
+
+        return $taskCreated->response()->setStatusCode(201);
+    }
+
+    public function destroy(TaskDeleteRequest $request){
+        $taskDeleted = $this->service->destroy($request);
+
+        if(!$taskDeleted) return response(['error'=>'task does not was deleted'])->setStatusCode(401);
+
+        return response(['message'=>'task deleted with success'])->setStatusCode(200);
+    }
+
+    public function update(TaskUpdateRequest $request){
+        $taskUpdated = $this->service->update($request);
+
+        if(!$taskUpdated) return response(['error'=>'task does not was updated'])->setStatusCode(403);
+
+        return response(['message'=>'task updated with success'])->setStatusCode(200);
+    }
+
+    public function updateStatus(TaskUpdateStatusRequest $request){
+        $taskUpdated = $this->service->updateStatus($request);
+
+        if(!$taskUpdated) return response(['error'=>'status task does not was updated'])->setStatusCode(403);
+
+        return response(['message'=>'status task updated with success'])->setStatusCode(200);
+    }
 }
