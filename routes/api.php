350e59e3faef683e5a7dbc06e269cfea2be0b398
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-10-22 22:57:41 -0300

    Transferencia das rotas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::delete('/tasks', [TaskController::class, 'destroy']);
+Route::put('/tasks', [TaskController::class, 'update']);
+Route::put('/tasks/status', [TaskController::class, 'updateStatus']);
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::post('/items', [ItemController::class, 'store']);
+Route::put('/items', [ItemController::class, 'update']);
+Route::put('/items/status', [ItemController::class, 'updateStatus']);
+Route::delete('/items', [ItemController::class, 'destroy']);
