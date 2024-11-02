8ab7e51410eac168b95bbb865e7ba23352bef835
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-02 00:03:21 -0300

    Criação da função de registrar usuário

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -15,6 +16,16 @@ class AuthCOntroller extends Controller
     }
 
     public function register(RegisterRequest $request){
+        $user = $this->user->create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
 
+        $token = $user->createToken('auth-token')->plainTextToken;
+        $user->token = $token;
+
+        $resource = new UserResource($user);
+        return $resource->response()->setStatusCode(201);
     }
 }
