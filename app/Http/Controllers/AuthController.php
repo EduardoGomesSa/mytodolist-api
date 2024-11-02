314cc7c6892b4d24cd5da8589080957c8bce1ef7
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-02 00:35:08 -0300

    Criação das rotas de register, login, validateToken e logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class AuthCOntroller extends Controller
+class AuthController extends Controller
 {
     private User $user;
 
--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\TaskController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/validate-token', [AuthController::class, 'validateToken']);
+
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::delete('/tasks', [TaskController::class, 'destroy']);
+    Route::put('/tasks', [TaskController::class, 'update']);
+    Route::put('/tasks/status', [TaskController::class, 'updateStatus']);
+
+    Route::get('/items', [ItemController::class, 'index']);
+    Route::post('/items', [ItemController::class, 'store']);
+    Route::put('/items', [ItemController::class, 'update']);
+    Route::put('/items/status', [ItemController::class, 'updateStatus']);
+    Route::delete('/items', [ItemController::class, 'destroy']);
 });
 
 
 
-Route::get('/tasks', [TaskController::class, 'index']);
-Route::post('/tasks', [TaskController::class, 'store']);
-Route::delete('/tasks', [TaskController::class, 'destroy']);
-Route::put('/tasks', [TaskController::class, 'update']);
-Route::put('/tasks/status', [TaskController::class, 'updateStatus']);
 
-Route::get('/items', [ItemController::class, 'index']);
-Route::post('/items', [ItemController::class, 'store']);
-Route::put('/items', [ItemController::class, 'update']);
-Route::put('/items/status', [ItemController::class, 'updateStatus']);
-Route::delete('/items', [ItemController::class, 'destroy']);
