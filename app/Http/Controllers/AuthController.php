cf26f166dc32057e60a21644653f7bf7d0c7f1fc
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-11-02 00:14:03 -0300

    Criação do método de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthCOntroller extends Controller
 {
     private User $user;
 
-    public function __construct(User $user) {
+    public function __construct(User $user)
+    {
         $this->user = $user;
     }
 
-    public function register(RegisterRequest $request){
+    public function register(RegisterRequest $request)
+    {
         $user = $this->user->create([
             'name' => $request->name,
             'email' => $request->email,
@@ -27,5 +31,23 @@ class AuthCOntroller extends Controller
 
         $resource = new UserResource($user);
         return $resource->response()->setStatusCode(201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = $this->user->where('email', $request->email)->first();
+
+        if (!$user) return response(['error' => 'e-mail informado nao esta cadastrado']);
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            $token = $user->createToken('auth-token')->plainTextToken;
+            $user->token = $token;
+
+            $resource = new UserResource($user);
+
+            return $resource->response()->setStatusCode(200);
+        }
+
+        return response(['error'=>'senha informada esta incorreta']);
     }
 }
